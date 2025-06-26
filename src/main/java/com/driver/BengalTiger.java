7eb4a6ef5501b3057b9cf1a8ef0c1524927bc1c8
Gautam kumar <165561578+gautamkumar83431@users.noreply.github.com>
2025-06-26 22:30:20 +0530

    Update BengalTiger.java

--- src/main/java/com/driver/BengalTiger.java
+++ src/main/java/com/driver/BengalTiger.java
@@ -1,6 +1,17 @@
 package com.driver;
 
 public class BengalTiger implements Tiger {
+    private String color;
+    private double averageWeight;
+    private String preferredClimate;
+
+    public BengalTiger(String color, double averageWeight, String preferredClimate) {
+        this.color = color;
+        this.averageWeight = averageWeight;
+        this.preferredClimate = preferredClimate;
+    }
+
+    
     @Override
     public String getType() {
         return "Bengal Tiger";
